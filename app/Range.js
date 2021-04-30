bd1237b1d7f02fc09b9fadef1766a1978c8890ac
developerlin123 <developerlin123@gmail.com>
2021-04-30 10:23:56 +0200

    setup css autofix

---
+++ app/Range.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Component } from "react";
+
+class Range extends Component{
+    constructor(){
+        super()
+
+    }
+
+
+    render(){
+
+        return(
+            <div className="range">
+                haha
+            </div>
+        )
+    }
+}
+
+export default Range;
---
+++ app/main.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {render} from "react-dom"
+import Range from "./Range"
+
+render(
+    <div>
+        <Range />
+    </div>,
+    document.getElementById("container")
+)
