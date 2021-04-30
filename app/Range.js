c6dfcfeda8c5a3faacecb0e9fd98552400e2f58d
developerlin123 <developerlin123@gmail.com>
2021-04-30 12:29:30 +0200

    add scale line

--- app/Range.js
+++ app/Range.js
@@ -2,17 +2,59 @@ import React from "react"
 import { Component } from "react";
 
 class Range extends Component{
-    constructor(){
+    constructor({width,min,max}){
         super()
+        this.bigUnitAmount = Math.ceil(width/60);
+        this.bigUnitWidth = width / this.bigUnitAmount;
+        this.smallUnitWidth =  this.bigUnitWidth / 5;
 
+        this.perbigUnitNumber = parseInt((max-min)/this.bigUnitAmount);
+        this.persmallUnitNumber = parseInt(this.perbigUnitNumber/5);
+ 
+        console.log(this)
     }
 
+    showScaleLine(){
+        var scalelines=[];
+        var length=this.bigUnitAmount*5+1;
+        for(var i=0;i<length;i++){
+            scalelines.push(<i key={i}></i>)
+        }
+
+        return scalelines;
+    }
+
+    componentDidMount(){
+       $(this.refs.range).find(".scaleline i").css("margin-right",this.smallUnitWidth-1);
+       $(this.refs.range).find(".scaleline i").eq(-2).css("margin-right",this.smallUnitWidth-2);
+       $(this.refs.range).find(".scaleline i").eq(-1).css("margin-right",0);
+       $(this.refs.range).find(".scaleline i:nth-child(5n+1)").addClass("big");
+    } 
 
     render(){
 
         return(
-            <div className="range">
-                haha
+            <div className="range" ref="range" style={{width:this.props.width}} >
+                <div className="bar" style={{width:this.props.width}}></div>
+                <div className="scaleline">
+                    {this.showScaleLine()}
+                    {/* <i className="b"></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i className="b"></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i className="b"></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i className="b"></i> */}
+                </div>
             </div>
         )
     }
--- app/main.js
+++ app/main.js
@@ -2,9 +2,14 @@ import React from "react"
 import {render} from "react-dom"
 import Range from "./Range"
 
+let props={
+    "width":555,
+    "min":100,
+    "max":5000,
+}
 render(
     <div>
-        <Range />
+        <Range {...props} />
     </div>,
     document.getElementById("container")
 )
